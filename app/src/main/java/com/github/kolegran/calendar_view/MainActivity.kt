de6129cc39bb2e5f06aa12db6fbcf44ddd4d12d3
kolegran <kolegran@gmail.com>
2023-01-28 18:44:14 +0200

    Created simple calendar

--- app/src/main/java/com/github/kolegran/calendar_view/MainActivity.kt
+++ app/src/main/java/com/github/kolegran/calendar_view/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.github.kolegran.calendar_view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.kolegran.calendar_view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupCalendar()
+    }
+
+    private fun setupCalendar() {
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
